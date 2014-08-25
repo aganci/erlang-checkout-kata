174f72aa3aafad28b8ee59e7f100fc5ed2dad7fb
Antonio Ganci <aganci@gmail.com>
2014-08-25 12:28:31 +0200

    AA

--- tests/checkout_tests.erl
+++ tests/checkout_tests.erl
@@ -5,5 +5,8 @@ checkout_test() ->
 	?assertEqual(0,   checkout:price("")),
 	?assertEqual(50,  checkout:price("A")),
 	?assertEqual(80,  checkout:price("AB")),
-	?assertEqual(115, checkout:price("CDBA")).
+	?assertEqual(115, checkout:price("CDBA")),
+
+	?assertEqual(100, checkout:price("AA")).
+
 
